bee1c413dce27b1392cff6218da36cd316fae9e0
Stojda <alexstojda@outlook.com>
2021-01-05 22:40:47 -0500

    fix: can translate sessions & streams

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Translatable\HasTranslations;
 
 class Session extends Model
 {
     use HasFactory;
+    use HasTranslations;
+
+    public $translatable = [
+        'title',
+        'description',
+    ];
 
     protected $fillable = [
         'title',
--- app/Models/SessionStream.php
+++ app/Models/SessionStream.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use App\Support\Zoom;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class SessionStream extends Model
 {
     use HasFactory;
+
+    use HasTranslations;
+
+    public $translatable = [
+        'title',
+        'description',
+    ];
 
     protected $fillable = [
         'title',
