2849cdbb472ee5eb464814b9e754340b97f4ed6b
Stojda <alexstojda@outlook.com>
2021-03-07 23:25:06 -0500

    fix: Update sidebar with discord link message

--- resources/views/user/sidebar.blade.php
+++ resources/views/user/sidebar.blade.php
@@ -1,8 +1,15 @@
 <div class="bg-white rounded overflow-hidden shadow-lg mx-4">
-    <div class="text-center p-6  border-b">
-        <p class="pt-2 text-lg">{{ __('Welcome to CSE 2021,') }}<br/>{{ Auth::user()->name }}</p>
+    <div class="text-center pt-4 pb-2 px-6">
+        <p class="pt-2 text-lg">{{ __('Welcome to').' '.config('app.name').',' }}<br/>{{ Auth::user()->name }}</p>
     </div>
-    <div class="border-b">
+    @if(Auth::user()->discord_user_id == null)
+        <a href="{{ route('discord') }}" class="block m-3 bg-blue-100 rounded-md">
+            <p class="p-2 font-semibold">
+                {{ __('Link your Discord account to get important updates and information regarding the conference!') }}
+            </p>
+        </a>
+    @endif
+    <div class="border-b border-t">
         <a href="{{ route('sessions') }}" class="px-4 py-2 hover:bg-gray-100 flex">
             <div class="text-gray-800">
                 <i class="fas fa-calendar-day"></i>
@@ -18,18 +25,18 @@
             </div>
             <div class="pl-3">
                 <p class="text-sm font-medium text-gray-800 leading-none">{{ __('Discord') }}</p>
-                <p class="text-xs text-gray-500">{{ __('Access the CSE 2021 discord') }}</p>
+                <p class="text-xs text-gray-500">{{ __('Access the discord for').' '.config('app.name') }}</p>
             </div>
         </a>
-{{--        <a href="{{ route('links') }}" class="px-4 py-2 hover:bg-gray-100 flex">--}}
-{{--            <div class="text-gray-800">--}}
-{{--                <i class="fas fa-folder"></i>--}}
-{{--            </div>--}}
-{{--            <div class="pl-3">--}}
-{{--                <p class="text-sm font-medium text-gray-800 leading-none">{{ __('Resources') }}</p>--}}
-{{--                <p class="text-xs text-gray-500">{{ __('Conference Resources') }}</p>--}}
-{{--            </div>--}}
-{{--        </a>--}}
+        {{--        <a href="{{ route('links') }}" class="px-4 py-2 hover:bg-gray-100 flex">--}}
+        {{--            <div class="text-gray-800">--}}
+        {{--                <i class="fas fa-folder"></i>--}}
+        {{--            </div>--}}
+        {{--            <div class="pl-3">--}}
+        {{--                <p class="text-sm font-medium text-gray-800 leading-none">{{ __('Resources') }}</p>--}}
+        {{--                <p class="text-xs text-gray-500">{{ __('Conference Resources') }}</p>--}}
+        {{--            </div>--}}
+        {{--        </a>--}}
         <a href="{{ route('account') }}" class="px-4 py-2 hover:bg-gray-100 flex">
             <div class="text-gray-800">
                 <i class="fas fa-user"></i>
@@ -39,11 +46,5 @@
                 <p class="text-xs text-gray-500">{{ __('Modify my account') }}</p>
             </div>
         </a>
-    </div>
-
-    <div class="">
-        {{--        <a href="#" class="px-4 py-4 hover:bg-gray-100 flex">--}}
-        {{--            <p class="text-sm font-medium text-gray-800 leading-none">{{ __('Logout') }}</p>--}}
-        {{--        </a>--}}
     </div>
 </div>
