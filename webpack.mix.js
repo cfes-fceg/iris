42e62b7a65f5ebf5650fed0dd9326bd110bce2fd
Stojda <alexstojda@outlook.com>
2021-03-10 22:34:15 -0500

    fix: mix versions

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,7 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('tailwindcss'),
     require('autoprefixer'),
 ]).sass('resources/scss/style.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
